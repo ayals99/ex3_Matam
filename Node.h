bc10882d9bc3e5526be3702d1cef749ad24d5424
ayals99 <ayal.stein@campus.technion.ac.il>
2023-06-01 21:53:13 +0300

    starting Queue.h

---
+++ Node.h
@@ -0,0 +1,75 @@
+#ifndef EX3_MATAM_NODE_H
+#define EX3_MATAM_NODE_H
+
+
+template<class S>
+class Node{
+    typedef Node& NodePtr;
+
+private:
+    S& m_item;
+    NodePtr m_next;
+    NodePtr m_previous;
+
+public:
+    /**
+     * @description: Constructor for Node
+     * @param: node
+     * @return: item of the node
+     */
+    Node():m_item(nullptr), m_next(nullptr), m_previous(nullptr){}
+
+    /**
+     * @description: Constructor for Node
+     * @param: item to insert to the node
+     * @note: the item is copied, not inserted itself into the node
+     * @return: Node
+     */
+    explicit Node(S* item): m_next(nullptr), m_previous(nullptr){
+        m_item = new S(*item);
+    }
+
+    /**
+     * @description: Copy Constructor for Node
+     * @param: other node to copy
+     * @return: Node
+     */
+    Node(const Node& other){
+        this->m_item = new S(*getItem(other));
+        this->m_next = getNext(other);
+        this->m_previous = getPrevious(other);
+    }
+
+    /**
+     * @description: Destructor for Node
+     */
+    ~Node() {
+        delete m_item;
+    }
+
+
+
+    /** Getters */
+
+    /**
+     * @description: Getter the item of the node
+     * @param: node
+     * @return: item of the node
+     */
+    S& getItem(NodePtr node) const {
+        return m_item;
+    }
+
+    /**
+     * @description: Getter for the next node
+     * @param: node
+     * @return: next node
+     */
+    NodePtr getNext(NodePtr node) const {
+        return m_next;
+    }
+};
+
+
+
+#endif
--- Queue.h
+++ Queue.h
@@ -1,11 +1,115 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include "Node.h"
 #include <iostream>
-#include <stdexcept>
 
+static int EMPTY = 0;
+
+template<class T>
 class Queue{
+    typedef Node<T> queueNode;
 
+private:
+    queueNode m_head;
+    int m_size;
+
+public:
+    /**
+     * @brief: Constructor for Queue
+     */
+    Queue(): m_head(EMPTY), m_size(0){}
+
+    class iterator{
+
+    private:
+        queueNode* m_pointer;
+        friend class Queue<T>;
+
+    public:
+        queueNode* operator*() const {
+            return *m_pointer;
+        }
+
+        queueNode* operator++() const{
+            assert(*this == nullptr);
+            return getNext(m_pointer);;
+        }
+
+        queueNode* operator++(int) const {
+            assert(*this == nullptr);
+            return getNext(m_pointer);;
+        }
+
+        bool operator==(const iterator& other) const {
+            return m_pointer == other.m_pointer;
+        }
+
+        bool operator!=(const iterator& other) const {
+            return m_pointer != other.m_pointer;
+        }
+        //TODO: iterator needs to be finished
+    };
+
+    /**
+     * @param: item to insert to the
+     *
+     * @return reference to the queue, so we can concatenate functions
+     *
+     * @note: the item is copied, not inserted itself into the queue
+     */
+    Queue& pushBack(const T toInsert){
+        queueNode nodeToPush = new queueNode(toInsert);
+        queueNode lastElement = findLast(m_head);
+        insertAfter(nodeToPush, lastElement);
+        m_size++;
+        return *this;
+    }
+
+    /**
+     * @param: queue
+     *
+     * @return reference to first element of the queue
+     */
+    queueNode front(const Queue& queue) const{
+        return m_head;
+    }
+
+    /**
+     * @param: queue
+     *
+     * @description: removes the first element of the queue
+     *
+     * @return none
+     */
+    void popFront(Queue& queue){
+        queueNode firstElement = front(queue);
+        m_head = getNext(firstElement);
+        delete firstElement;
+        m_size--;
+    }
+
+    /**
+     * @param: queue
+     *
+     * @return number of elements in the queue
+     */
+    int size(Queue& queue){
+        return m_size;
+    }
+
+    Queue& filter(Queue& queue){
+
+    }
+
+    void transform(Queue& queue, void (*transformFunc)(T&)){
+        queueNode currentNode = m_head;
+        while(currentNode != nullptr){
+            T& item = getItem(currentNode);
+            transformFunc(item);
+            currentNode = getNext(currentNode);
+        }
+    }
 };
 
 
